4996141405ede010f49203457fd2d7487333d592
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-28 15:59:29 +0530

    post and put method in angualr

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { NgToastService } from 'ng-angular-popup';
 import { Observable } from 'rxjs';
 import { NotificationService } from './notification.service'
@@ -11,19 +11,28 @@ export class HttpService {
 
   constructor(private http: HttpClient, private toastr: NgToastService) { }
   // getRequest(url: string): Observable<>{
+  // getRequest
   getRequest(url: string): any {
     this.http.get(url).subscribe(
       (response) => {
         this.toastr.success({ detail: `response`, summary: 'Response Success', duration: 3000 });
         console.log(response);
+        return response;
       },
       (error) => {
         console.error(error);
         this.toastr.error({ detail: 'response', summary: 'Response Success', duration: 3000 });
-      }
-    )
+      });
   }
-  postRequest(url: string, option: HttpParams): Observable<any> {
+
+  //PostRequest
+  postRequest(url: string, data: any, option?: any): Observable<any> {
     return this.http.post(url, option);
+  }
+
+  // putRequest 
+  updateRequest(url: string, data: any, option?: any): Observable<any> {
+    return this.http.put(url, data, option);
+
   }
 }
